e9190eb30f7953cfc13b0be16118ce60e676b8f1
Yousef Alsaeid <macshiba@mac.com>
2018-11-26 18:59:14 +0300

    submission one

--- restaurants/views.py
+++ restaurants/views.py
@@ -6,14 +6,32 @@ def welcome(request):
 def restaurant_list(request):
 
     context = {
-
+    	"my_list" : [
+    		{
+    			"restaurant_name": "Burger King" ,
+    			"food_type": "Fast Food" 
+    		},
+    		{
+    			"restaurant_name": "Pizza Hut" ,
+    			"food_type": "Italian"
+    		},
+    		{
+    			"restaurant_name": "Villa Firuze" ,
+    			"food_type": "Libanese" 
+    		}
+    	]
     }
+
     return render(request, 'list.html', context)
 
 
 def restaurant_detail(request):
 
     context = {
+    	"my_object" : {
+    		"restaurant_name" : "Villa Firuze",
+    		"food_type" : "Libanese"
+    	}
 
     }
     return render(request, 'detail.html', context)
